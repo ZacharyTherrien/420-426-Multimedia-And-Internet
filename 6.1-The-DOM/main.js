715c997a9e8991c03296bf26c3c03c1b25758993
Zachary Therrien <1727873@johnabbottcollege.net>
2019-11-04 09:42:31 -0500

    Beginning AJAX
    Added work 6.1 and Assignment 3.

--- 6.1-The-DOM/main.js
+++ 6.1-The-DOM/main.js
@@ -1,87 +1,90 @@
 const mountains = [
-    {'name': 'Mount Everest', 'elevation': 8848, 'range': 'Himalayas', 'location': 'Nepal/China'},
-    {'name': 'K2', 'elevation': 8611, 'range': 'Karakoram', 'location': 'India - Highest in India'},
-    {'name': 'Kangchenjunga', 'elevation': 8586, 'range': 'Himalayas', 'location': 'Nepal/India'},
-    {'name': 'Lhotse', 'elevation': 8516, 'range': 'Himalayas', 'location': 'Nepal/China - Climbers ascend the face of Lhotse when climbing Everest'},
-    {'name': 'Makalu', 'elevation': 8485, 'range': 'Himalayas', 'location': 'Nepal/China'},
-    {'name': 'Cho Oyu', 'elevation': 8201, 'range': 'Himalayas', 'location': 'Nepal/China - Considered easiest eight-thousander'},
-    {'name': 'Dhaulagiri', 'elevation': 8167, 'range': 'Himalayas', 'location': 'Nepal - Presumed world\'s highest from 1808-1838'},
-    {'name': 'Manaslu', 'elevation': 8163, 'range': 'Himalayas', 'location': 'Nepal'},
-    {'name': 'Nanga Parbat', 'elevation': 8126, 'range': 'Himalayas', 'location': 'Pakistan'},
-    {'name': 'Annapurna', 'elevation': 8091, 'range': 'Himalayas', 'location': 'Nepal - First eight-thousander to be climbed (1950)'},
-    {'name': 'Gasherbrum I', 'elevation': 8080, 'range': 'Karakoram', 'location': 'Pakistan/China - Originally named K5'},
-    {'name': 'Broad Peak', 'elevation': 8051, 'range': 'Karakoram', 'location': 'Pakistan/China - Originally named K3'},
-    {'name': 'Gasherbrum II', 'elevation': 8035, 'range': 'Karakoram', 'location': 'Pakistan/China - Originally named K4'},
-    {'name': 'Shishapangma', 'elevation': 8027, 'range': 'Himalayas', 'location': 'China'},
-]
+	{ name: 'Mount Everest', elevation: 8848, range: 'Himalayas', location: 'Nepal/China' },
+	{ name: 'K2', elevation: 8611, range: 'Karakoram', location: 'India - Highest in India' },
+	{ name: 'Kangchenjunga', elevation: 8586, range: 'Himalayas', location: 'Nepal/India' },
+	{ name: 'Lhotse', elevation: 8516, range: 'Himalayas', location: 'Nepal/China - Climbers ascend the face of Lhotse when climbing Everest' },
+	{ name: 'Makalu', elevation: 8485, range: 'Himalayas', location: 'Nepal/China' },
+	{ name: 'Cho Oyu', elevation: 8201, range: 'Himalayas', location: 'Nepal/China - Considered easiest eight-thousander' },
+	{ name: 'Dhaulagiri', elevation: 8167, range: 'Himalayas', location: 'Nepal - Presumed world\'s highest from 1808-1838' },
+	{ name: 'Manaslu', elevation: 8163, range: 'Himalayas', location: 'Nepal' },
+	{ name: 'Nanga Parbat', elevation: 8126, range: 'Himalayas', location: 'Pakistan' },
+	{ name: 'Annapurna', elevation: 8091, range: 'Himalayas', location: 'Nepal - First eight-thousander to be climbed (1950)' },
+	{ name: 'Gasherbrum I', elevation: 8080, range: 'Karakoram', location: 'Pakistan/China - Originally named K5' },
+	{ name: 'Broad Peak', elevation: 8051, range: 'Karakoram', location: 'Pakistan/China - Originally named K3' },
+	{ name: 'Gasherbrum II', elevation: 8035, range: 'Karakoram', location: 'Pakistan/China - Originally named K4' },
+	{ name: 'Shishapangma', elevation: 8027, range: 'Himalayas', location: 'China' }
+];
 
-//Create the table initially.
-var Table = document.getElementById('mountain-data');
-var numRows = 0;
-var Rows;
+const buildMountainsTable = mountainsList => {
+	const tableBody = document.getElementById('mountain-data');
+	
+	tableBody.innerHTML = ''; // Clear any old data.
+	
+	mountainsList.map(mountain => {
+		const row = document.createElement('tr');
+	
+		const nameCell = document.createElement('td');
+		const elevationCell = document.createElement('td');
+		const rangeCell = document.createElement('td');
+		const locationCell = document.createElement('td');
+	
+		const nameText = document.createTextNode(mountain.name);
+		const elevationText = document.createTextNode(mountain.elevation);
+		const rangeText = document.createTextNode(mountain.range);
+		const locationText = document.createTextNode(mountain.location);
+	
+		nameCell.appendChild(nameText);
+		elevationCell.appendChild(elevationText);
+		rangeCell.appendChild(rangeText);
+		locationCell.appendChild(locationText);
+	
+		row.appendChild(nameCell);
+		row.appendChild(elevationCell);
+		row.appendChild(rangeCell);
+		row.appendChild(locationCell);
+	
+		tableBody.appendChild(row);
+	});
+};
 
-for(let i = 0; i < mountains.length; i++)
-    InsertRow(i);
+const filterMountainsHigherThanInput = () => {
+	const elevation = document.getElementById('height-filter').value;
+	return mountains.filter(mountain => mountain.elevation > elevation);
+};
 
-var displayMax = document.getElementById('max');
-var displayMin = document.getElementById('min');
-var displayAVG = document.getElementById('avg');
+// The following 3 function expressions return the result of the reduce method on a given array of mountains.
+const findHighestMountain = mountainsList => mountainsList.reduce((accumulator, currentMountain) => accumulator > currentMountain.elevation ? accumulator : currentMountain.elevation, 0);
 
-//Display Min, Max and AVG.
-var total = mountains[0].elevation, min = mountains[0].elevation, max = mountains[0].elevation;
+const findShortestMountain = mountainsList => mountainsList.reduce((accumulator, currentMountain) => accumulator < currentMountain.elevation ? accumulator : currentMountain.elevation, mountainsList[0].elevation);
 
-for(var i = 1; i < mountains.length; i++){
-    if(mountains[i].elevation > max)
-        max = mountains[i].elevation;
-    else if(mountains[i].elevation < min)
-        min = mountains[i].elevation;
+const findAverageMountainElevation = mountainsList => mountainsList.reduce((accumulator, currentMountain) => accumulator += currentMountain.elevation, 0) / mountainsList.length;
 
-    total += mountains[i].elevation;
-}
-
-displayMax.append(max);
-displayMin.append(min);
-displayAVG.append(total/mountains.length);
-
-//Based on input, onky display mountains in table greater than the input.
 // This function will fire when the button in the HTML is clicked.
-var FirstClick = true;
-
 function calculate() {
-    var minHeight = document.getElementById('height-filter').value;
+	const filteredMountains = filterMountainsHigherThanInput();
+	
+	buildMountainsTable(filteredMountains);
 
-    //Add rows based on if it's the first time or not.
-    if(FirstClick){
-        for(let i = 0; i < mountains.length; i++){
-            document.getElementById('mountain-data').deleteRow(0);  
-        }
-        FirstClick = false;
-    }   
-    else
-        for(let i = mountains.length - 1; i >= (mountains.length - numRows); i--)
-            document.getElementById('mountain-data').deleteRow(0);
+	const highestMountain = findHighestMountain(filteredMountains);
+	const shortestMountain = findShortestMountain(filteredMountains);
+	const averageMountain = findAverageMountainElevation(filteredMountains);
+	
+	const highestMountainElementNode = document.getElementById('highest-mountain');
+	const shortestMountainElementNode = document.getElementById('shortest-mountain');
+	const averageMountainElementNode = document.getElementById('average-height');
+	
+	const highestMountainTextNode = document.createTextNode(`The tallest mountain is: ${highestMountain}`);
+	const shortestMountainTextNode = document.createTextNode(`The shortest mountain is: ${shortestMountain}`);
+	const averageMountainTextNode = document.createTextNode(`The average height of all the mountains is: ${averageMountain}`);
 
-    numRows = 0;
-
-    //Add the rows, only allowing the ones with an elevation greater than input.
-    for(let i = 0; i < mountains.length; i++)
-        if(mountains[i].elevation > minHeight){
-            InsertRow(i);
-            numRows = numRows + 1;
-        }
+	highestMountainElementNode.innerHTML = '';
+	shortestMountainElementNode.innerHTML = '';
+	averageMountainElementNode.innerHTML = '';
+	
+	highestMountainElementNode.appendChild(highestMountainTextNode);
+	shortestMountainElementNode.appendChild(shortestMountainTextNode);
+	averageMountainElementNode.appendChild(averageMountainTextNode);
 }
 
-//Function to insert rows into the table.
-function InsertRow(index){
-    Rows = Table.insertRow(index);
-
-    var cell1 = Rows.insertCell(0);
-    var cell2 = Rows.insertCell(1);
-    var cell3 = Rows.insertCell(2);
-    var cell4 = Rows.insertCell(3);
-
-    cell1.innerHTML = mountains[index].name;
-    cell2.innerHTML = mountains[index].elevation;
-    cell3.innerHTML = mountains[index].range;
-    cell4.innerHTML = mountains[index].location;
-}
+buildMountainsTable(mountains);
+calculate();
---
+++ Assignment-3-ZACHARY-THERRIEN/main.js
@@ -0,0 +1,14 @@
+const url = "https://api.spacexdata.com/v3/"; // The root of the SpaceX API
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        doStuff(data)
+    })
+    .catch(function(error) {
+        // If there is any error you will catch them here.
+        console.log(error);
+});
+function doStuff(data) {
+    // If the request was successful then data will have everything you asked for.
+    console.log(data);
+}
