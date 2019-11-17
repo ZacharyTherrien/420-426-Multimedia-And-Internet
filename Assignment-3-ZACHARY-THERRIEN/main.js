fa46453ac3d5d2961f661ed7eda491387b5e9145
Zachary Therrien <ztherrien.work@gmail.com>
2019-11-17 02:00:00 -0500

    Updated Assignment to current version
    The progress has been updated to what has been done at school. Which includes the addition of the Upcoming Launch.

--- Assignment-3-ZACHARY-THERRIEN/main.js
+++ Assignment-3-ZACHARY-THERRIEN/main.js
@@ -1,14 +1,29 @@
-const url = "https://api.spacexdata.com/v3/"; // The root of the SpaceX API
-fetch(url)
+const urlUpcoming = "https://api.spacexdata.com/v3/launches/next";// The root of the SpaceX API
+fetch(urlUpcoming)
     .then(response => response.json())
     .then(data => {
-        doStuff(data)
+        handleUpcoming(data);
     })
     .catch(function(error) {
-        // If there is any error you will catch them here.
+        let UpcomingDateNodeError = document.getElementById("UpcomingDate");
+        let UpcomingVehicleNodeError = document.getElementById("UpcomingVehicle");
+        UpcomingDateNodeError.appendChild("error.")
+        UpcomingVehicleNodeError.appendChild("error.")
         console.log(error);
 });
-function doStuff(data) {
+function handleUpcoming(NextLaunch) {
     // If the request was successful then data will have everything you asked for.
-    console.log(data);
+    console.log(NextLaunch);
+
+    //Get the nodes.
+    let UpcomingDateNode = document.getElementById("UpcomingDate");
+    let UpcomingVehicleNode = document.getElementById("UpcomingVehicle");
+
+    //Create text nodes & populate the text nodes with data for upcoming launches.
+    let UpcomingDateText = document.createTextNode(NextLaunch.launch_date_unix);
+    let UpcomingVehicleText = document.createTextNode(NextLaunch.rocket.rocket_name);
+
+    //Append text nodes to the nodes of the h3.
+    UpcomingDateNode.appendChild(UpcomingDateText);
+    UpcomingVehicleNode.appendChild(UpcomingVehicleText);
 }
